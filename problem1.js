3fe8af950a35bf79706024ef1135268dba96bba1
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-12-15 23:33:36 -0600

    update.

--- problem1.js
+++ problem1.js
@@ -1,3 +1,5 @@
+//Write a JavaScript function to calculate the factorial of a number.
+
 function factorial(n) {
     if (n === 1 || n === 0) {
       return 1;
---
+++ problem2.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function to compute the sum of an array of integers.
+
+function sumArray(arr) {
+    if (arr.length === 1) {
+      return arr[0];
+    }
+    else {
+      return arr.pop() + sumArray(arr);
+    }
+  };
+  
+  console.log(sumArray([1,2,3,4,5,6]));
